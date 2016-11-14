3a1d92a7ae7f3fcdba1de53c8ff8940da3dd20c9
hville <hugov@runbox.com>
2016-11-14 14:59:42 +0800

    check alternative structure

--- elm.js
+++ elm.js
@@ -1,3 +1,6 @@
+//master: 1844 char,3.9kbm,1.6kbz
+//factory: 3109 char,4,25kbm,1.8kbz
+//refac: 2024, 4.0,1.6kbz
 var dom = require('dom-document'),
 		decorators = require('./decorators'),
 		namespaces = require('./namespaces'),
@@ -53,7 +56,7 @@ function parseArgument(arg, idx) {
 			if (is.string(arg)) return parse(arg)
 			if (is.function(arg)) return {element: arg()}
 			if (is.node(arg)) return {element: arg}
-		case 1: // second optional argument is an configuration object
+		case 1: // second optional argument is a configuration object
 			if (is.object(arg)) return arg
 		default: // all arguments after the optional element and config are children
 			if (Array.isArray(arg)) return {children: arg}
