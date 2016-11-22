e06f2f2bb47e829c7373f84fcc6db51de9e6c83a
hville <hugov@runbox.com>
2016-11-22 22:24:27 +0800

    intermediate utils no longer exported

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var global = require('dom-document'),
-		createElement = require('./src/create-element'),
 		factory = require('./src/create-factory'),
 		decorators = require('./src/decorators'),
-		setChildren = require('./src/set-children'),
-		decorate = require('./src/decorate'),
-		namespaces = require('./src/namespaces'),
-		mergeKeys = require('./src/merge-keys'),
-		parseArgument = require('./src/parse-argument'),
-		is = require('./src/is')
+		namespaces = require('./src/namespaces')
 
 var el = factory()
 el.svg = factory({xmlns: 'http://www.w3.org/2000/svg'})
@@ -16,11 +10,5 @@ module.exports = {
 	el: el,
 	global: global,
 	namespaces: namespaces,
-	decorators: decorators,
-	decorate: decorate,
-	createElement: createElement,
-	mergeKeys: mergeKeys,
-	parseArgument: parseArgument,
-	is: is,
-	setChildren: setChildren
+	decorators: decorators
 }
