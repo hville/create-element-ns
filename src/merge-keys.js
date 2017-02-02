2eb862bd56e125f719b7ce4e47ce4cabc0b8df18
hville <hugov@runbox.com>
2017-02-02 15:22:39 +0800

    removed Object assign

--- src/merge-keys.js
+++ src/merge-keys.js
@@ -9,7 +9,7 @@ function mergeKeys(t, s) {
 				v = s[k]
 		t[k] = Array.isArray(v) ? flatConcat(t[k] || [], v)
 			: is.node(v) ? v.cloneNode(true)
-			: is.object(v) ? Object.assign(t[k] || {}, v)
+			: is.object(v) ? assign(t[k] || {}, v)
 			: v
 	}
 	return t
@@ -18,4 +18,8 @@ function flatConcat(arr, val) {
 	if (Array.isArray(val)) for (var i=0; i<val.length; ++i) flatConcat(arr, val[i])
 	else arr.push(val)
 	return arr
+}
+function assign(tgt, src) {
+	for (var i=0, ks=Object.keys(src); i<ks.length; ++i) tgt[ks[i]] = src[ks[i]]
+	return tgt
 }
