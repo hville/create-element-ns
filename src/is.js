dd875ae6f18ca3a4401580aac98b456b15b8fe4a
hville <hugov@runbox.com>
2017-02-02 15:23:02 +0800

    simplification

--- src/is.js
+++ src/is.js
@@ -1,22 +1,17 @@
 module.exports = {
-	node: isNode,
-	function: isFunction,
-	string: isString,
-	object: isObject,
-	stringlike: stringLike
-}
-function isNode(node) {
-	return node && node.nodeName && node.nodeType > 0 && node.cloneNode
-}
-function isObject(obj) {
-	return obj && ((obj.constructor === Object) || (!obj.constructor && typeof obj === 'object'))
-}
-function isString(str) {
-	return str && str.constructor === String
-}
-function stringLike(val) {
-	return (val && (val.constructor === String || val.constructor === Number)) || val === 0
-}
-function isFunction(fcn) {
-	return fcn && fcn.constructor === Function
+	node: function isNode(node) {
+		return node && node.nodeName && node.nodeType > 0 && node.cloneNode
+	},
+	function: function isFunction(fcn) {
+		return fcn && fcn.constructor === Function
+	},
+	string: function isString(str) { //ignores the empty string
+		return str && str.constructor === String
+	},
+	object: function isObject(obj) {
+		return obj && ((obj.constructor === Object) || (!obj.constructor && typeof obj === 'object'))
+	},
+	stringlike: function stringLike(val) { //ignores the empty string
+		return val ? (val.constructor === String || val.constructor === Number) : val === 0
+	}
 }
